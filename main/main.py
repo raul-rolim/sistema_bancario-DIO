f120a97b5e59cce09be07e0f069ae60c55f820b9
raul-rolim <rauldelucaroberto@gmail.com>
2024-06-10 23:29:03 -0300

    Desafio Concluido

--- main/main.py
+++ main/main.py
@@ -1,17 +1,8 @@
-# #EXTRATO
-# 	-Positional (saldo) and Keyword (extrato)
-#
-# #CRIAR USUÁRIO (CLIENTE)
-# 	-Armazenar em uma lista (nome,nascimento,cpf e endereço 	(logradouro, numero - bairro - cidade/sigla estado)
-# 	-Apenas um cadastro por CPF
-#
-# #CRIAR CONTA CORRENTE
-# 	-Lista: agência, número da conta e usuário
-# 	-Número da conta é sequencial, iniciando em 1
-# 	-Agência é fixo: 0001
-# 	-O usuário pode ter mais de uma conta, mas deve pertencer a apenas 	um usuário cada conta.
-
-
+usuario = {'nome': None,
+               'cpf': None,
+               'data_nascimento': None,
+               'endereço': None
+               }
 
 def menu():
     menu = """
@@ -32,24 +23,31 @@ def main():
     global numero_conta
     global usuario
     global conta_corrente
+
     numero_conta = 1
     usuarios = []
-    contas = []
 
 
     while True:
-        usuario_logado = usuario["cpf"] is not None
+        usuario_logado = usuarios != []
         opcao = menu()
         if opcao == 'd' and usuario_logado:
             valor_a_depositar = float(input("Insira o valor que deseja depositar: "))
-            depositar(valor_a_depositar, conta_corrente["saldo"], conta_corrente["extrato"])
+            conta_corrente["saldo"], conta_corrente["extrato"] = depositar(valor_a_depositar,
+                                                                           conta_corrente["saldo"],
+                                                                           conta_corrente["extrato"])
 
         elif opcao == 's' and usuario_logado:
             valor_a_sacar = float(input("Insira o valor que deseja sacar: "))
-            saque(valor_sacado=valor_a_sacar, saldo=conta_corrente["saldo"], extrato=conta_corrente["extrato"], conta_corrente=conta_corrente)
+            conta_corrente["saldo"], conta_corrente["extrato"] = saque(valor_sacado=valor_a_sacar,
+                                                                       saldo=conta_corrente["saldo"],
+                                                                       extrato=conta_corrente["extrato"],
+                                                                       conta_corrente=conta_corrente)
+
+
 
         elif opcao == 'e' and usuario_logado:
-            extrato(conta_corrente["saldo"], conta_corrente["nome"], extrato=conta_corrente["extrato"])
+            extrato(conta_corrente["saldo"], conta_corrente["usuario"]["nome"], extrato=conta_corrente["extrato"])
 
         elif opcao == 'nc':
             criar_conta(usuarios)
@@ -69,6 +67,7 @@ def depositar(valor_depositado, saldo, extrato, /):
         saldo += valor_depositado
         extrato += f"Depósito: R$ {valor_depositado:.2f}\n"
         print("Depósito realizado com sucesso!\n")
+        return saldo, extrato
     else:
         print("Não foi possível efetuar o depósito. O valor inserido é inválido")
 
@@ -83,6 +82,7 @@ def saque(*, valor_sacado, saldo, extrato, conta_corrente):
         extrato += f"Saque: R$ {valor_sacado:.2f}\n"
         conta_corrente["numero_saques"] += 1
         print("Saque realizado com sucesso!\n")
+        return saldo, extrato
 
     elif saldo < valor_sacado:
         print("Não foi possível efetuar o saque. Saldo Insuficiente")
@@ -132,7 +132,7 @@ def verificar_usuario(usuarios, cpf):
 
     return False
 
-def criar_conta(usuario):
+def criar_conta(usuarios):
     global numero_conta
     global conta_corrente
     LIMITE_SAQUES = 3
@@ -141,10 +141,11 @@ def criar_conta(usuario):
     limite_por_saque = 500
     extrato = ""
     numero_saques = 0
-    if usuario['cpf'] is None:
+    if not usuarios:
         print("Você ainda não se cadastrou!")
     else:
-        conta_corrente = {'usuario':usuario,
+        #usar numero conta -1 como index para criações
+        conta_corrente = {'usuario':usuarios[0],
                           'saldo':saldo,
                           'extrato':extrato,
                           'limite_por_saque':limite_por_saque,
